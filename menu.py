dd39d61f6a8bf312c75a43d19428ce00d15c19f9
Ruan Carlos <ruan.exerciciodatascience@gmail.com>
2022-12-07 16:18:18 -0300

    snake war

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ from pygame.locals import *
 from random import randint, choice
 from sys import exit
 import time
+import numpy as np
 import os
 
 
@@ -35,6 +36,13 @@ cooldown = 0
 vezes = 0
 seg = 2
 minutos = 59
+
+# Logicas mais basicas
+pega = False
+quem = ""
+
+# Mapa
+murada = False
 
 # Lógica
 pause = False
@@ -91,14 +99,16 @@ class Snake(pygame.sprite.Sprite):
     def __init__(self, pos_inicial, up=False, down=False, left=False, right=True):
         pygame.sprite.Sprite.__init__(self)
 
+        self.pontos_d = 0
         self.pontos = 0
+
         self.R = 0
         self.up = 0
         self.right = 1
         self.down = 2
         self.left = 3
         self.snake = [(pos_inicial, 300), (pos_inicial + 10, 300), (pos_inicial + 20, 300), (pos_inicial + 30, 300), (pos_inicial + 50, 300)]
-        self.vida_T = len(self.snake)
+        self.vida_T = 5
         self.snake_skin = pygame.Surface((10, 10))
         
         if up == True:
@@ -162,14 +172,47 @@ class Snake(pygame.sprite.Sprite):
         window.blit(nickname, (x, y))
 
     def upsup(self):
-        self.vida_T += 4
+        self.vida_T += 5
         self.snake_skin.fill((randint(10, 240), randint(10, 240), randint(10, 240)))
-        self.snake.append((0, 0))
+        for i in range(0, 5):
+            self.snake.append((0, 0))
     
     def update(self):
+        
         self.vida_T += 1
         self.snake_skin.fill((randint(10, 240), randint(10, 240), randint(10, 240)))
         self.snake.append((0, 0))
+        
+    
+    def perda(self):
+        if len(self.snake) > 5:
+            for i in range(0, 5):
+                self.vida_T -= 1
+                del self.snake[-1]
+
+        elif len(self.snake) > 4:
+            for i in range(0, 4):
+                self.vida_T -= 1
+                del self.snake[-1]
+            
+        elif len(self.snake) > 3:
+            for i in range(0, 3):
+                self.vida_T -= 1
+                del self.snake[-1]
+        
+        elif len(self.snake) > 2:
+            for i in range(0, 2):
+                self.vida_T -= 1
+                del self.snake[-1]
+        
+        elif len(self.snake) > 1:
+            for i in range(0, 1):
+                self.vida_T -= 1
+                del self.snake[-1]
+        
+        else: 
+            pass
+        
 
 
 
@@ -183,7 +226,7 @@ class Itens(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (x, y))
         self.rect = self.image.get_rect()
         self.pos = self.on_grid_random()
-        self.sorteio = randint(0, 100)
+        self.sorteio = randint(0, 20)
 
     
     def blit(self, tela):
@@ -207,6 +250,17 @@ class Game():
     
     def imagem(self, tela, x=0, y=0):
         tela.blit(self.bg, (x, y))
+
+
+# Função Para Colisão
+def colision(c1, c2):
+    a = False
+    b = False
+    for muro in c2[0]:
+        for mur in c2[1]:
+            if muro == c1[0] and mur == c1[1]:
+                return True
+
 
 # Funções de Auxilio
 def contagem():
@@ -293,19 +347,19 @@ def mapa():
     pedreira_blur = Game(imagem=os.path.join(mapas, "pedreira_blur.jpg"))
 
     if sorteio > 0 and sorteio <= 30:
-        return [gardem, "GARDEM", gardem_blur]
+        return [gardem, "GARDEM", gardem_blur, 5]
     
     elif sorteio > 30 and sorteio <= 60:
-        return [pedreira, "STONES", pedreira_blur]
+        return [pedreira, "STONES", pedreira_blur, 10]
     
     elif sorteio > 60 and sorteio <= 90:
-        return [desert, "DESERT", desert_blur]
+        return [desert, "DESERT", desert_blur, 15]
     
     elif sorteio > 90 and sorteio <= 120:
-        return [campo, "FOOTBALL", campo_blur]
+        return [campo, "FOOTBALL", campo_blur, 20]
     
     elif sorteio > 120 and sorteio <= 150:
-        return [beach, "BEACH", beach_blur]
+        return [beach, "BEACH", beach_blur, 25]
 
 
 
@@ -313,10 +367,11 @@ def mapa():
 mapa_escolhido = mapa()
 
 def game_restart():
-    global exec, segundos, contador, mapa_escolhido, vezes, minutos, seg, marcador, cobra1, itens, cobra2
+    global exec, segundos, contador, pega, quem, mapa_escolhido, vezes, minutos, parede_drop, murada, seg, marcador, caver, cobra1, itens, cobra2, maca_dourada
     mapa_escolhido = mapa()
     vezes = 0
     minutos = 2
+    murada = False
     seg = 59
     exec = 0
     segundos = 0
@@ -325,7 +380,11 @@ def game_restart():
     cobra1 = Snake(30)
     maca = Itens(os.path.join(sprites, "maçã.png"), 10, 10)
     cobra2 = Snake(x - 30, left=True, right=False)
-
+    maca_dourada = Itens(os.path.join(sprites, "maçã_dourada.png"), 10, 10)
+    parede_drop = Itens(os.path.join(sprites, "parede_drop.png"), 10, 10)
+    caver = Itens(os.path.join(sprites, "caver.png"), 10, 10)
+    pega = False
+    quem = ""
 
 
 # Game caracteristicas
@@ -337,9 +396,28 @@ pygame.display.set_icon(icone)
 
 
 #  Personagens
+caver = Itens(os.path.join(sprites, "caver.png"), 10, 10)
 maca = Itens(os.path.join(sprites, "maçã.png"), 10, 10)
+maca_dourada = Itens(os.path.join(sprites, "maçã_dourada.png"), 10, 10)
 cobra1 = Snake(30)
 cobra2 = Snake(x - 30, left=True, right=False)
+parede_drop = Itens(os.path.join(sprites, "parede_drop.png"), 10, 10)
+
+# Stone
+muros = pygame.image.load(os.path.join(sprites, "muro_pedra.png"))
+muro1 = (np.arange(70, 81), np.arange(50, 351))
+muro2 = (np.arange(403, 414), np.arange(50, 225))
+muro3 = (np.arange(70, 678), np.arange(50, 61))
+
+muro4 = (np.arange(514, 514+97), np.arange(516, 527))
+muro5 = (np.arange(599, 610), np.arange(434, 434+96))
+
+muro6 = (np.arange(864, 875), np.arange(204, 204+354))
+
+muro7 = (np.arange(1079, 1090), np.arange(560, 560+967))
+muro8 = (np.arange(1079, 1079+97), np.arange(645, 656))
+
+muros_list = [muro1, muro2, muro3, muro4, muro5, muro6, muro7, muro8]
 
 # Loop Caracters
 clock = pygame.time.Clock()
@@ -351,6 +429,7 @@ while run:
     # Tela de inicio
     if inicio == True:
 
+        
         # Sempre que voltar a tela inicial o game restarta
         game_restart()
         mouse = pygame.mouse.get_pos()
@@ -406,8 +485,6 @@ while run:
         # Update
         # Animação da Tela
         var -= speed_geral
-    
-
 
 
     # Tela de game iniciado
@@ -523,7 +600,7 @@ while run:
             timing()
     
             # Vida da Cobra 1 painel
-            cobra1.vida(25, 20, 60, window)
+            cobra1.vida(25, 190, 20, window)
             if cobra1.vida_T == 1:
                 cobra1.R = 255
             else:
@@ -532,7 +609,7 @@ while run:
 
 
             # Vida da Cobra 2 painel
-            cobra2.vida(25, x - 100, 60, window)
+            cobra2.vida(25, x - 100, 20, window)
             if cobra2.vida_T == 1:
                 cobra2.R = 255
             else:
@@ -553,29 +630,155 @@ while run:
 
 
             # Aparecer na tela
+            if  mapa_escolhido[3] == 10:
+                if parede_drop.sorteio % 4 == 0:
+                    parede_drop.blit(window)
+
+
             cobra1.blit(window)
             cobra2.blit(window)
             maca.blit(window)
 
+            if maca_dourada.sorteio % 10 == 0:
+                maca_dourada.blit(window)
+
+            if caver.sorteio % 3 == 0:
+                caver.blit(window)
+            
+            if parede_drop.sorteio % 4 == 0:
+                parede_drop.blit(window)
 
             # Colisão
+            if murada == True:
+                window.blit(muros, (0, 0))
+                if pega == True:
+
+                    if quem == "cobra1":
+
+                        for muro in muros_list:
+                            if colision(cobra2.snake[0], muro):
+                                cobra2.vida_T -= 1
+                                if cobra2.vida_T > 0:
+                                    del cobra2.snake[-1]
+                            
+                                elif cobra2.vida_T <= 0:
+                                    game = False
+                                    tela_score = True
+                                
+                                if cobra2.direction == cobra2.up:
+                                    cobra2.direction = cobra2.down
+                                
+                                elif cobra2.direction == cobra2.down:
+                                    cobra2.direction = cobra2.up
+                                
+                                elif cobra2.direction == cobra2.left:
+                                    cobra2.direction = cobra2.right
+                                
+                                elif cobra2.direction == cobra2.right:
+                                    cobra2.direction = cobra2.left
+                
+                    elif quem == "cobra2":
+                        for muro in muros_list:
+                            if colision(cobra1.snake[0], muro):
+                                cobra1.vida_T -= 1
+                                if cobra1.vida_T > 0:
+                                    del cobra1.snake[-1]
+                            
+                                elif cobra1.vida_T <= 0:
+                                    game = False
+                                    tela_score = True
+                                
+                                if cobra1.direction == cobra1.up:
+                                    cobra1.direction = cobra1.down
+                                
+                                elif cobra1.direction == cobra1.down:
+                                    cobra1.direction = cobra1.up
+                                
+                                elif cobra1.direction == cobra1.left:
+                                    cobra1.direction = cobra1.right
+                                
+                                elif cobra1.direction == cobra1.right:
+                                    cobra1.direction = cobra1.left
+           
+           
+            # Parede drop
+            if cobra1.colide(cobra1.snake[0], parede_drop.pos):
+
+                    parede_drop.pos = parede_drop.on_grid_random()
+                    parede_drop.sorteio = randint(0, 20)
+                    pega = True
+                    quem = "cobra1"
+                    murada = True
+            
+            elif cobra2.colide(cobra2.snake[0], parede_drop.pos):
+
+                    parede_drop.pos = parede_drop.on_grid_random()
+                    parede_drop.sorteio = randint(0, 20)
+                    pega = True
+                    quem = "cobra2"
+                    murada = True
+
             # Cobra1 com a maçã
-            if cobra1.colide(cobra1.snake[0], maca.pos):
+            elif cobra1.colide(cobra1.snake[0], maca.pos):
                 apple.play()
+                
+                if maca_dourada.sorteio % 10 != 0:
+                    maca_dourada.sorteio = randint(0, 20)
+                
+                if caver.sorteio % 3 != 0:
+                    caver.sorteio = randint(0, 20)
+                
+                if  mapa_escolhido[3] == 10:
+                    if parede_drop.sorteio % 4 != 0:
+                        parede_drop.sorteio = randint(0, 20)
+
                 cobra1.pontos += 1
                 maca.pos = maca.on_grid_random()
-                maca.sorteio = randint(0, 100)
                 cobra1.update()
+            
+            elif cobra1.colide(cobra1.snake[0], maca_dourada.pos):
+                apple.play()
+                cobra1.pontos_d += 1
+                maca_dourada.pos = maca_dourada.on_grid_random()
+                maca_dourada.sorteio = randint(0, 20)
+                cobra1.upsup()
+            
+
+            elif cobra1.colide(cobra1.snake[0], caver.pos):
+                caver.pos = caver.on_grid_random()
+                caver.sorteio = randint(0, 20)
+                cobra1.perda()
             
 
             # Cobra2 com a maçã
             elif cobra2.colide(cobra2.snake[0], maca.pos):
                 apple.play()
+
+                if maca_dourada.sorteio % 10 != 0:
+                    maca_dourada.sorteio = randint(0, 20)
+
+                if caver.sorteio % 3 != 0:
+                    caver.sorteio = randint(0, 20)
+                
+                if  mapa_escolhido[3] == 10:
+                    if parede_drop.sorteio % 4 != 0:
+                        parede_drop.sorteio = randint(0, 20)
+
                 cobra2.pontos += 1
                 maca.pos = maca.on_grid_random()
-                maca.sorteio = randint(0, 100)
                 cobra2.update()
-        
+            
+            elif cobra2.colide(cobra2.snake[0], maca_dourada.pos):
+                apple.play()
+                cobra2.pontos_d += 1
+                maca_dourada.pos = maca_dourada.on_grid_random()
+                maca_dourada.sorteio = randint(0, 20)
+                cobra2.upsup()
+
+            elif cobra2.colide(cobra2.snake[0], caver.pos):
+                caver.pos = caver.on_grid_random()
+                caver.sorteio = randint(0, 20)
+                cobra2.perda()
             
 
             # Cabeça com cabeça
@@ -585,6 +788,8 @@ while run:
                     del cobra1.snake[-4:-1]
                     cobra1.vida_T -= 3
                 else:
+                    cobra1.vida_T - 3
+                    cobra2.vida_T - 3
                     game = False
                     tela_score = True
                 
@@ -593,6 +798,8 @@ while run:
                     del cobra2.snake[-4:-1]
                     cobra2.vida_T -= 3
                 else:
+                    cobra1.vida_T - 3
+                    cobra2.vida_T - 3
                     game = False
                     tela_score = True
             
@@ -833,6 +1040,10 @@ while run:
             elif cobra1.pontos < cobra2.pontos:
                 vencedor = nick2
                 perdedor = nick1
+        
+            else:
+                vencedor = nick1
+                perdedor = nick2
 
 
 
@@ -886,26 +1097,45 @@ while run:
         # Labels da tela
         label1 = pygame.font.SysFont(os.path.join(fonts, "Aleo-BoldItalic.ttf"), 70)
         label2 = pygame.font.SysFont(os.path.join(fonts, "Aleo-BoldItalic.ttf"), 40)
+        label3 = pygame.font.SysFont(os.path.join(fonts, "Aleo-BoldItalic.ttf"), 30)
 
         win = label1.render("Vencedor", True, (17,6,38))
         loss = label1.render("Perdedor", True, (17,6,38))
 
         if vencedor == nick1:
+            macas_d1 = label3.render(f"Maçãs Douradas Comidas - {cobra1.pontos_d}", True, (17,6,38))
+            macasd1_rect = macas_d1.get_rect()
+            macas_d2 = label3.render(f"Maçãs Douradas Comidas - {cobra2.pontos_d}", True, (17,6,38))
+
+
             macas1 = label2.render(f"Maçãs Comidas - {cobra1.pontos}", True, (17,6,38))
             macas_rect = macas1.get_rect()
             macas2 = label2.render(f"Maçãs Comidas - {cobra2.pontos}", True, (17,6,38))
+            
+            
+            vida1 = label2.render(f"Vida - {len(cobra1.snake)}", True, (17,6,38))
+            vida1_rect = vida1.get_rect()
+            vida2 = label2.render(f"Vida - {len(cobra2.snake)-1}", True, (17,6,38))
         
         elif vencedor == nick2:
+            macas_d1 = label3.render(f"Maçãs Douradas Comidas - {cobra2.pontos_d}", True, (17,6,38))
+            macasd1_rect = macas_d1.get_rect()
+            macas_d2 = label3.render(f"Maçãs Douradas Comidas - {cobra1.pontos_d}", True, (17,6,38))
+
+
             macas1 = label2.render(f"Maçãs Comidas - {cobra2.pontos}", True, (17,6,38))
             macas_rect = macas1.get_rect()
             macas2 = label2.render(f"Maçãs Comidas - {cobra1.pontos}", True, (17,6,38))
+            
+            vida1 = label2.render(f"Vida - {len(cobra2.snake)}", True, (17,6,38))
+            vida1_rect = vida1.get_rect()
+            vida2 = label2.render(f"Vida - {len(cobra1.snake)-1}", True, (17,6,38))
+
 
         nome_w = label2.render(f"{vencedor}", True, (17,6,38))
         nome_wrect = nome_w.get_rect()
         nome_l = label2.render(f"{perdedor}", True, (17,6,38))
         nome_lrect = nome_l.get_rect()
-
-
 
 
         placa = pygame.image.load(os.path.join(sprites, "score.png"))
@@ -917,13 +1147,17 @@ while run:
             window.blit(placa, (195, 45))
             window.blit(win, (308, 80))
             window.blit(nome_w, ((195 + 450 / 2) - (nome_wrect[2] / 2), 150))
-            window.blit(macas1, ((195 + 450 / 2) - (macas_rect[2] / 2), 300))
+            window.blit(macas1, ((195 + 450 / 2) - (macas_rect[2] / 2), 320))
+            window.blit(vida1, (195 + 450 / 2 - vida1_rect[2] / 2, 270))
+            window.blit(macas_d1, (195 + 450 / 2 - macasd1_rect[2] / 2, 370))
         else:
             placa = pygame.transform.scale(placa, (400, 600))
             window.blit(placa, (220, 60))
             window.blit(win, (310, 95))
             window.blit(nome_w, ((220 + 400 / 2) - (nome_wrect[2] / 2), 165))
-            window.blit(macas1, ((220 + 400 / 2) - (macas_rect[2] / 2), 315))
+            window.blit(macas1, ((220 + 400 / 2) - (macas_rect[2] / 2), 335))
+            window.blit(vida1, (220 + 400 / 2 - vida1_rect[2] / 2, 285))
+            window.blit(macas_d1, (220 + 400 / 2 - macasd1_rect[2] / 2, 385))
         
         
         if mouse[0] >= 660 and mouse[0] <= 1060 and mouse[1] >= 60 and mouse[1] <= 660:
@@ -931,16 +1165,19 @@ while run:
             window.blit(placa, (645, 45))
             window.blit(loss, (758, 80))
             window.blit(nome_l, ((645 + 450 / 2) - (nome_lrect[2] / 2), 150))
-            window.blit(macas2, ((645 + 450 / 2) - (macas_rect[2] / 2), 300))
+            window.blit(macas2, ((645 + 450 / 2) - (macas_rect[2] / 2), 320))
+            window.blit(vida2, (645 + 450 / 2 - vida1_rect[2] / 2, 270))
+            window.blit(macas_d2, (645 + 450 / 2 - macasd1_rect[2] / 2, 370))
 
         else:
             placa = pygame.transform.scale(placa, (400, 600))
             window.blit(placa, (660, 60))
             window.blit(loss, (750, 95))
             window.blit(nome_l, ((660 + 400 / 2) - (nome_lrect[2] / 2), 165))
-            window.blit(macas2, ((660 + 400 / 2) - (macas_rect[2] / 2), 315))
+            window.blit(macas2, ((660 + 400 / 2) - (macas_rect[2] / 2), 335))
+            window.blit(vida2, (660 + 400 / 2 - vida1_rect[2] / 2, 285))
+            window.blit(macas_d2, (660 + 400 / 2 - macasd1_rect[2] / 2, 385))
 
-
-        print("Shoto", cobra1.vida_T, "Julio", cobra2.vida_T)
+    
     clock.tick(30)
     pygame.display.update()
